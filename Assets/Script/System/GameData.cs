43925fe551fb6a58dfd3d566c411346af74f4641
mutelnwza <mutemallow@gmail.com>
2024-10-13 19:52:08 +0700

    stats

--- Assets/Script/System/GameData.cs
+++ Assets/Script/System/GameData.cs
@@ -8,11 +8,21 @@ public class GameData
 {
     public string Name;
     public Dictionary<string, int> inventoryData;
+    public int life;
+    public int strength;
+    public int defense;
+    public int luck;
+    public int statPoints;
 
     public GameData()
     {
         Name = "Player";
         inventoryData = new Dictionary<string, int>();
+        life = 3;
+        strength = 1;
+        defense = 1;
+        luck = 1;
+        statPoints = 0;
     }
 
 }
---
+++ Assets/Script/System/PlayerManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, IDataPersistence
+{
+    #region Stats
+
+    public int Life { get; private set; }
+    public int StatPoints { get; private set; }
+    private Dictionary<string, int> stats;
+
+    #endregion
+    private void Awake()
+    {
+        stats = new Dictionary<string, int>
+        {
+            { "Strength", 0 },
+            { "Defense", 0 },
+            { "Luck", 0 }
+        };
+    }
+
+
+    public void LoadData(GameData data)
+    {
+        Life = data.life;
+        StatPoints = data.statPoints;
+        stats["Strength"] = data.strength;
+        stats["Defense"] = data.defense;
+        stats["Luck"] = data.luck;
+
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.life = Life;
+        data.statPoints = StatPoints;
+        data.strength = stats["Strength"];
+        data.defense = stats["Defense"];
+        data.luck = stats["Luck"];
+
+    }
+
+    //เพิ่มปุ่ม
+    public void AddPoint()
+    {
+        if (StatPoints > 0)
+        {
+            System.Random random = new System.Random();
+            List<string> statList = stats.Keys.ToList();
+            string randomStat = statList[random.Next(statList.Count)];
+            stats[randomStat] += 1;
+            StatPoints -= 1;
+        }
+    }
+
+    public void Die()
+    {
+        Life-=1;
+    }
+
+}
