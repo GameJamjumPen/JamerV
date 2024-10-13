b6f569024b91c84d79e8f55222c08b54d70f5523
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-10-14 00:13:55 +0700

    Complete inventorySystem

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -1,11 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public CardSO[] cardSOs;
-    public void AddItem(CardSO card){
-        Debug.Log(card._cardName);
+    #region Declare Variable
+    public KeyCode openCloseKey = KeyCode.Tab;
+    public GameObject backPackSystem;
+    public GameObject pocketSystem;
+
+    [Header("Slots")]
+    public CardSO cardSelected;
+    public InventorySlot[] itemSlots;
+    public InventorySlot[] actualSlots;
+
+    [Header("Display")]
+    public TextMeshProUGUI cardName;
+    public Image cardImage;
+    public Image cardType;
+    [Tooltip("Types[0] will be ATK Types[1] will be DEF card")]
+    public Sprite[] types;
+
+    #endregion
+    public void Update(){
+        if(Input.GetKeyDown(openCloseKey)){
+            if(backPackSystem.activeSelf){
+                backPackSystem.SetActive(false);
+            }else backPackSystem.SetActive(true);
+        }
     }
+    // public void Update(){
+    //     if()
+    // }
+    public void AddItem(CardSO _card){
+        for(int i = 0;i < itemSlots.Length;i++){
+            if(!itemSlots[i].isFull){
+                itemSlots[i].AddItem(_card);
+                return;
+            }
+        }
+    }
+    #region SlotSelected
+    public void DeselectedAllSlot(){
+        foreach (InventorySlot slot in itemSlots)
+        {
+            slot.OnDeselected();
+        } 
+    }
+
+    public void DisplaySelected(){
+        cardName.text = cardSelected._cardName;
+        cardImage.sprite = cardSelected._cardSprite;
+        if(cardSelected.cardType == CardType.ATK){
+            cardType.sprite = types[0];
+        }else{
+            cardType.sprite = types[1];
+        }
+    }
+    #endregion
+
+
 }
--- Assets/Script/InventorySlot.cs
+++ Assets/Script/InventorySlot.cs
@@ -3,22 +3,134 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using Unity.VisualScripting;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour , IPointerClickHandler , IBeginDragHandler , IDragHandler , IEndDragHandler
 {
     //ITEM DATA//
     public CardSO cardSO;
-    public bool _isFull;
+    public bool isFull;
 
     //ITEM SLOT//
     [SerializeField]
     private Image slotImage;
+    public GameObject selectedShader;
+    public bool isSelected;
+    //[Header("Reference")]
+    private Inventory inventory;
 
-    public void AddItem(CardSO cardSO){
-        this.cardSO = cardSO;
-        _isFull = true;
+    [Header("Drag and Drop")]
+
+    public Transform beforeDragPos;
+    public InventorySlot inventorySlot;
+    void Awake()
+    {
+        inventory = FindObjectOfType<Inventory>();
     }
+    #region Add/Remove Item
+    public void AddItem(CardSO cardSO){
+        slotImage.gameObject.SetActive(true);
+        this.cardSO = cardSO;
+        isFull = true;
+        UpdateDisplay();
+    }
+    public void RemoveItem(){
+        this.cardSO = null;
+        isFull = false;
+        UpdateDisplay();
+        OnDeselected();
+    }
+    #endregion
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left){
+            OnSelected();
+        }
+        if(eventData.button == PointerEventData.InputButton.Right){
+            OnDeselected();
+        }
+        if(eventData.button == PointerEventData.InputButton.Middle){
+            if(isSelected){
+                RemoveItem();
+            }
+        }        
+    }
+
     public void UpdateDisplay(){
-        slotImage.sprite = cardSO._cardSprite;
+        if(cardSO != null){
+            slotImage.sprite = cardSO._cardSprite;
+        }else{
+            slotImage.gameObject.SetActive(false);
+        }
+    }
+    #region Selected/Deselected Slot
+    public void OnSelected(){
+        inventory.DeselectedAllSlot();
+        selectedShader.SetActive(true);
+        isSelected = true;
+        if(this.cardSO != null){
+            inventory.cardSelected = this.cardSO;
+            inventory.DisplaySelected();
+        }
+    }
+    public void OnDeselected(){
+        selectedShader.SetActive(false);
+        isSelected = false;
+    }
+    #endregion
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // DraggableObject.gameObject.SetActive(true);
+        inventorySlot = null;
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(!isSelected){
+            return;
+        }else{
+            slotImage.transform.position = Input.mousePosition;
+        }
+        // List to hold all raycast hits
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        // Raycast for UI elements under the mouse
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Iterate through the results to find an InventorySlot component
+        foreach (RaycastResult result in results)
+        {
+            // Try to get the InventorySlot component on the hit object;
+            InventorySlot slot;
+            if (result.gameObject.TryGetComponent<InventorySlot>(out slot))
+            {
+                // If the InventorySlot component is found, assign it and break
+                inventorySlot = slot;
+                Debug.Log("InventorySlot found: " + inventorySlot);
+                break;
+            }
+        }
+        if (inventorySlot == null)
+        {
+            Debug.Log("No InventorySlot found under mouse.");
+        }
+        //Debug.Log("Dragging");
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(isSelected && cardSO != null){
+            if(inventorySlot != null && !inventorySlot.isFull){
+                inventorySlot.AddItem(cardSO);
+                RemoveItem();
+                Debug.Log("Added");
+            }
+        }
+        Debug.Log("End Drag");
+        slotImage.transform.position = beforeDragPos.transform.position;
     }
 }
