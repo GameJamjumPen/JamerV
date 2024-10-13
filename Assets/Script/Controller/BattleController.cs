b56db04074b60fab178d8a52fd6fe753ddf96d21
Tanakit <caldarianz@gmail.com>
2024-10-13 14:54:19 +0700

    TurnBase is now playable

--- Assets/Script/Controller/BattleController.cs
+++ Assets/Script/Controller/BattleController.cs
@@ -7,16 +7,16 @@ public class BattleController : MonoBehaviour
 {   
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
-    public BattleModel battleModel;
-    public BattleView battleView;
+    private BattleModel battleModel;
+    private BattleView battleView;
     public bool isPlayerTurn = true;
 
     void Start() {
         CharacterModel player = new CharacterModel("Player",100,10);
         CharacterModel enemy = new CharacterModel("Enemy",100,8);
 
-        GameObject enemmyInstance = Instantiate(enemyPrefab,new Vector2(10,0),quaternion.identity);
-        GameObject playerInstance = Instantiate(playerPrefab,new Vector2(5,0),quaternion.identity);
+        GameObject enemmyInstance = Instantiate(enemyPrefab,new Vector3(5,-2,0),quaternion.identity);
+        GameObject playerInstance = Instantiate(playerPrefab,new Vector3(-5,-2,0),quaternion.identity);
 
         CharacterView enemyView = enemmyInstance.GetComponent<CharacterView>();
         CharacterView playerView = playerInstance.GetComponent<CharacterView>();
@@ -24,13 +24,14 @@ public class BattleController : MonoBehaviour
         battleModel = new BattleModel(player,enemy);
 
         battleView = new BattleView(playerView,enemyView);
-        battleView.playerView.setName(player.name);
-        battleView.enemyView.setName(enemy.name);
+        battleView.playerView.setName(player.Name);
+        battleView.enemyView.setName(enemy.Name);
         battleView.UpdatePlayerView(player);
         battleView.UpdateEnemyView(enemy);
     }
     void Update() {
         if(Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("isPlayerTurn : " + isPlayerTurn.ToString());
             if(isPlayerTurn){
                 PlayerTurn();
             }
@@ -42,23 +43,27 @@ public class BattleController : MonoBehaviour
     public void PlayerTurn(){
         if(isPlayerTurn){
             battleModel.Attack(battleModel.player,battleModel.enemy);
+            Debug.Log("Enemy"+ battleModel.enemy.health);
             battleView.UpdateEnemyView(battleModel.enemy);
             if(battleModel.IsBattleOver()){
+                Debug.Log("Over");
                 return;
             }
             isPlayerTurn = false;
-            EnemyTurn();
+            Debug.Log("Finished PlayerTurn");
         }
     }
     public void EnemyTurn(){
         if(!isPlayerTurn){
             battleModel.Attack(battleModel.enemy,battleModel.player);
+            Debug.Log(battleModel.player.health);
             battleView.UpdatePlayerView(battleModel.player);
             if(battleModel.IsBattleOver()){
+                Debug.Log("Over");
                 return;
             }
             isPlayerTurn = true;
-            PlayerTurn();
+            Debug.Log("Finished EnemyTurn");
         }
     }
 }
